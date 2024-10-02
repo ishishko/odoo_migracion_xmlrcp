b8af772bd7069140f21369a714c10de6ddfd0453
Ignacio Shishko <shishkoignacio@gmail.com>
2024-10-02 12:09:29 -0300

    productos

--- cliente/geminis/maestro_Productos/maestro_product_product.py
+++ cliente/geminis/maestro_Productos/maestro_product_product.py
@@ -6,21 +6,23 @@ url_origen_g = "https://geminis.ntsystemwork.com.ar/"
 dbname_origen_g = "geminis"
 user_origen_g = "admin"
 pwd_origen_g = "gemi19nis"
+field_origen_g = ['name', 'image', 'image_medium', 'image_small']
 
-url_origen_r = "https://rake.ntsystemwork.com.ar/"
-dbname_origen_r = "rake_prod"
-user_origen_r = "admin"
-pwd_origen_r = "ra21ke"
+# url_origen_r = "https://rake.ntsystemwork.com.ar/"
+# dbname_origen_r = "rake_prod"
+# user_origen_r = "admin"
+# pwd_origen_r = "ra21ke"
 
-url_destino = "https://ortopediageminis-pruebas-pagos-12791939.dev.odoo.com/"
-dbname_destino = "ortopediageminis-pruebas-pagos-12791939"
+url_destino = "https://ortopediageminis.odoo.com/"
+dbname_destino = "ntsystemwork-cl-geminis17-14-0-12548781"
 user_destino = "admin"
-pwd_destino = "258c94152632f1bd8980e7b6d6f9ec9d3345e4cc"
+pwd_destino = "128fb3dfd05bc86e461ec8c09b10a59fdb4f107e"
+field_destino = ['name']
 
 # ========================================================================
-model_origen_g = "product.product"
-model_origen_r = "product.product"
-model_destino = "product.product"
+model_origen_g = "product.template"
+# model_origen_r = "product.product"
+# model_destino = "product.product"
 
 1
 clientes_origen_g = CreateXMLRCP(
@@ -39,11 +41,15 @@ cliente_destino = CreateXMLRCP(
 # cliente_destino.models_compare_fields(field_list_check_g)
 
 # 3
-data_g = clientes_origen_g.mass_read_data(field_list_check_g)
-data_g = clientes_origen_g.change_data_keys(data_g, 'image', 'image_1920')
-data_g = clientes_origen_g.change_data_keys(data_g, 'image_medium', 'image_1024')
-data_g = clientes_origen_g.change_data_keys(data_g, 'image_small', 'image_128')
+data_g = clientes_origen_g.mass_read_data(field_origen_g)
+# data_g = clientes_origen_g.change_data_keys(data_g, 'image', 'image_1920')
+# data_g = clientes_origen_g.change_data_keys(data_g, 'image_medium', 'image_1024')
+# data_g = clientes_origen_g.change_data_keys(data_g, 'image_small', 'image_128')
 # data_g = clientes_origen_g.change_data_keys(data_g, 'pos_categ_id', 'pos_categ_ids')
 
 # 4
-cliente_destino.mass_create_reg(data_g)
+# cliente_destino.mass_create_reg(data_g)
+data_d = cliente_destino.mass_read_data(field_destino)
+
+
+cliente_destino.models_update(data_g, data_d)
--- cliente/geminis/maestro_Productos/maestro_product_template.py
+++ cliente/geminis/maestro_Productos/maestro_product_template.py
@@ -37,27 +37,27 @@ model_origen_r = "product.template"
 model_destino = "product.template"
 
 # 1
-# clientes_origen_g = CreateXMLRCP(
-#     url_origen_g, dbname_origen_g, user_origen_g, pwd_origen_g, model_origen_g
-# )
+clientes_origen_g = CreateXMLRCP(
+    url_origen_g, dbname_origen_g, user_origen_g, pwd_origen_g, model_origen_g
+)
 
 # clientes_origen_g.models_use()
 # clientes_origen_g.models_fields_types(field_use_g)
 # clientes_origen_g.models_use(field_ignore_g + field_special_g)
 
 # 2
-# cliente_destino = CreateXMLRCP(
-#     url_destino, dbname_destino, user_destino, pwd_destino, model_origen_g
-# )
+cliente_destino = CreateXMLRCP(
+    url_destino, dbname_destino, user_destino, pwd_destino, model_origen_g
+)
 
 # cliente_destino.models_compare_fields(field_list_check_g)
 
 # 3
-# data_g = clientes_origen_g.mass_read_data(field_list_check_g)
-# data_g = clientes_origen_g.change_data_keys(data_g, 'image', 'image_1920')
-# data_g = clientes_origen_g.change_data_keys(data_g, 'image_medium', 'image_1024')
-# data_g = clientes_origen_g.change_data_keys(data_g, 'image_small', 'image_128')
-# # data_g = clientes_origen_g.change_data_keys(data_g, 'pos_categ_id', 'pos_categ_ids')
+data_g = clientes_origen_g.mass_read_data(field_list_check_g)
+data_g = clientes_origen_g.change_data_keys(data_g, 'image', 'image_1920')
+data_g = clientes_origen_g.change_data_keys(data_g, 'image_medium', 'image_1024')
+data_g = clientes_origen_g.change_data_keys(data_g, 'image_small', 'image_128')
+# data_g = clientes_origen_g.change_data_keys(data_g, 'pos_categ_id', 'pos_categ_ids')
 
 # 4
 # cliente_destino.mass_create_reg(data_g)
